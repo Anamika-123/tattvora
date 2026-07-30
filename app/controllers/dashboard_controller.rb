180bc2af4baca72ebaf81d32965978eab70011b4
Anamika Sharma <indorianamika@gmail.com>
2026-07-30 20:22:12 +0530

    Fixed review requested changes
    - refactor role based condition
    - Removed role method from user factory

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,14 +2,7 @@ class DashboardController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    case current_user.role
-    when "student"
-      redirect_to dashboard_student_path
-    when "teacher"
-      redirect_to dashboard_teacher_path
-    when "admin"
-      redirect_to dashboard_admin_path
-    end
+    redirect_to role_base_dashboard_path(current_user.role)
   end
 
   def student
--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -2,6 +2,5 @@ FactoryBot.define do
   factory :user do
     email { Faker::Internet.email }
     password { "Anamika@123" }
-    role { student }
   end
 end
